9d64f63209388a2c47702da1e74de8a0f301fcdd
edgarrps <edgarrps@gmail.com>
2023-05-20 23:38:31 -0400

    style and improvements of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-export default function App () {
+import Form from './components/Form'
+import Navbar from './components/Navbar'
+
+export default function App() {
   return (
-    <>Teste</>
+    <>
+      <Navbar />
+      <Form />
+    </>
   )
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,25 @@
+export default function Form() {
+  return (
+    <form className="flex justify-center items-center p-4 h-20 bg-stone-600 font-mono font-bold text-white">
+      <div className="flex space-x-8">
+        <div className="space-x-2">
+          <label>Item:</label>
+          <input className="pl-2 rounded-md bg-stone-300 w-40 h-7 text-stone-700" />
+        </div>
+
+        <div className="space-x-2">
+          <label>Qtd:</label>
+          <input
+            type="number"
+            min="1"
+            className="pl-2 rounded-md bg-stone-300 w-16 h-7 text-stone-700"
+          />
+        </div>
+
+        <button className="pl-2 pr-2 rounded-md shadow-md bg-stone-700 hover:bg-stone-500 duration-200">
+          Adicionar
+        </button>
+      </div>
+    </form>
+  )
+}
