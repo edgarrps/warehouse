2e89c3f1df2d8a2c0dd1f7162b6d1b85c4596aa5
edgarrps <edgarrps@gmail.com>
2023-07-02 19:39:52 -0400

    migration to next app

--- web/src/Routes.tsx
+++
@@ -1,9 +0,0 @@
-import { Routes, Route } from 'react-router-dom'
-import App from './App'
-export default function RoutesPage() {
-  return (
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  )
-}
--- web/src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="flex justify-center items-center h-[50px] w-screen bg-cyan-300">
-      <p className="font-bold font-mono hover:uppercase hover:text-stone-500 duration-300">
-        Warehouse
-      </p>
-    </nav>
-  )
-}
