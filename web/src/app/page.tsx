c513eb3db15c833cad054b49891794766d3c79f5
edgarrps <edgarrps@gmail.com>
2023-07-04 12:22:42 -0400

    insertion of input and button fields

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (<main className='h-screen bg-blue-200'>
-<div className='h-20 bg-blue-100'>Teste</div>
-</main>
+    <div className='h-[120px] bg-blue-100 font-mono text-center'>
+      <p className='pt-4 pb-2 text-blue-400'>WAREHOUSE</p>
+      <div className='flex justify-center space-x-4'>
+        <input className='pl-2 rounded-md bg-stone-100 border-4 border-blue-200 w-96'/>
+        <button className='w-8 rounded-md bg-blue-400 text-stone-200'>+</button>
+  </div>
+    </div>
+  </main>
   )
 }
