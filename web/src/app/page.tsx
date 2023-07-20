411e4cdf86aa50b146617e1732c5fd6cf9a460ec
edgarrps <edgarrps@gmail.com>
2023-07-20 12:52:48 -0400

    fetch db.json

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,30 @@
 "use client"
 
+interface warehouse {
+  id: number
+  name: string
+  amount: number
+  status: string
+}
+
+import { useEffect, useState } from "react"
+
 export default function Home() {
+  const [items, setItems] = useState<warehouse[]>([])
+  
+  useEffect(() => {
+    fetch("http://localhost:5000/db", {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((res) => res.json())
+      .then(data => {
+        setItems(data.warehouse)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (<main className='h-screen bg-blue-200'>
     <div className='h-[120px] bg-blue-100 font-mono text-center'>
       <p className='pt-4 pb-2 text-blue-400'>WAREHOUSE</p>
@@ -21,6 +45,7 @@ export default function Home() {
       <table className="bg-blue-400 w-[600px] rounded-xl">
         <thead>
           <tr className="text-xl bg-stone-100 rounded-2xl">
+            <th>ID</th>
             <th>ITEM</th>
             <th>QUANTITY</th>
             <th>STATUS</th>
@@ -28,30 +53,15 @@ export default function Home() {
           </tr>
         </thead>
         <tbody className=" text-stone-100">
-          <tr>
-            <td>MOUSE</td>
-            <td>2</td>
-            <td>OK</td>
-            <td>&#128465;</td>
-          </tr>
-          <tr>
-            <td>AXE</td>
-            <td>1</td>
-            <td>DAMAGED</td>
-            <td>&#128465;</td>
-          </tr>
-          <tr>
-            <td>CAP</td>
-            <td>1</td>
-            <td>OK</td>
-            <td>&#128465;</td>
-          </tr>
-          <tr>
-            <td>SUNGLASSES</td>
-            <td>2</td>
-            <td>OK</td>
-            <td>&#128465;</td>
-          </tr>
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.amount}</td>
+                <td>{item.status}</td>
+                <td>&#128465;</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
